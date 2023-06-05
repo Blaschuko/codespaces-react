cef339206c09ec40dfde7f0746af63b0927705b7
Blas Kozicki <69172685+Blaschuko@users.noreply.github.com>
2023-06-05 14:13:56 +0000

    main setup

--- src/App.js
+++ src/App.js
@@ -22,6 +22,10 @@ function App() {
           </a>
         </p>
       </header>
+      <div className="bg-blue-500 text-white p-4">
+            <h1 className="text-3xl font-bold">Hello, Tailwind!</h1>
+            <p className="mt-2">Tailwind CSS is awesome!</p> 
+        </div> 
     </div>
   );
 }
